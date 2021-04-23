fd4816a065eaf4a65c1fa193819d24b196426f49
Gustavo Lurial <gustavolurial@hotmail.com>
2021-04-23 18:33:04 -0300

    Atualizado o problema do select no banco. Telas de login e register funcionando e finalizadas.

--- StockWatcher/src/main/java/com/mycompany/stockwatcher/LoginController.java
+++ StockWatcher/src/main/java/com/mycompany/stockwatcher/LoginController.java
@@ -1,9 +1,70 @@
 package com.mycompany.stockwatcher;
+
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-   
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField txtPassword;
+    private Connection connection;
+    private Statement statement;
+    int c = 0;
+
+    @FXML
+    void acaoLoginBtn(ActionEvent event) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tradeworldbase", "root", "123");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("oie");
+
+        }
+        if (txtUsername.getText().equals("") || txtPassword.getText().equals("")) {
+            System.out.println("Preencha todos os campos");
+        } else {
+            try {
+                PreparedStatement stmt = connection.prepareStatement("SELECT *from usuario");
+                ResultSet result = stmt.executeQuery();
+                while (result.next()) {
+                    if ((result.getString("login").equals(txtUsername.getText())) && (result.getString("senha").equals(txtPassword.getText()))) {
+                        c++;
+                    }
+                }
+                if (c > 0) {
+                    statement.close();
+                    connection.close();
+                    App.setRoot("secondary");
+                } else {
+                    System.out.println("Usuário ou senha incorretos");
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
     @FXML
     private void switchToRegister() throws IOException {
         App.setRoot("TelaRegister");
---
+++ StockWatcher/src/main/java/com/mycompany/stockwatcher/TelaRegisterController.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.stockwatcher;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class TelaRegisterController implements Initializable {
+
+    private Connection connection;
+    private Statement statement;
+
+    @FXML
+    private TextField txtUserRegister;
+    @FXML
+    private TextField txtNameRegister;
+    @FXML
+    private PasswordField txtPassRegister;
+    @FXML
+    private PasswordField txtConPassRegister;
+    @FXML
+    private Button btnRegister;
+    @FXML
+    private Button btnVoltar;
+
+    String sql = "INSERT INTO usuario (nome, login, senha) values (?,?,?)";
+    //String sql2 = "SELECT login from usuario where login = 'gustavo'";
+    int c = 0;
+    int a = 0;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void acaoDoBotao(ActionEvent event) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tradeworldbase", "root", "123");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("oie");
+
+        }
+
+        if (txtUserRegister.getText().equals("") || txtNameRegister.getText().equals("") || txtPassRegister.getText().equals("") || txtConPassRegister.getText().equals("")) {
+            System.out.println("preencha todos os campos");
+        } else {
+            if (!txtPassRegister.getText().equals(txtConPassRegister.getText())) {
+                System.out.println("As senhas não estão iguais");
+            } else {
+
+                try {
+                    PreparedStatement stmt = connection.prepareStatement("SELECT *from usuario");
+                    ResultSet result = stmt.executeQuery();
+
+                    while (result.next()) {
+                        if (result.getString("login").equals(txtUserRegister.getText()) == true) {
+                            c++;
+                        }
+                    }
+                    if (c == 0) {
+                        a++;
+                        try {
+                            System.out.println("Usuario criado com sucesso!");
+                            PreparedStatement stm = connection.prepareStatement(sql);
+                            stm.setString(1, txtNameRegister.getText());
+                            stm.setString(2, txtUserRegister.getText());
+                            stm.setString(3, txtPassRegister.getText());
+                            stm.execute();
+                            stm.close();
+                            try {
+                                statement.close();
+                                connection.close();
+                            } catch (Exception ex) {
+                                System.out.println(ex);
+                            }
+                        } catch (SQLException ex) {
+                            System.out.println(ex);
+                        }
+                    } else {
+                        System.out.println("Usuario ja existe ANIMAL DE TETA");
+                        c = 0;
+                    }
+
+                } catch (SQLException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+        if(a>0){
+            
+            try {
+                App.setRoot("TelaLogin");
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    @FXML
+    private void switchToLogin() throws IOException {
+        App.setRoot("TelaLogin");
+    }
+}
